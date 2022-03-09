fa810b2b1b5855d1b7bed7cccd39894683a07015
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-03-09 11:27:54 +0200

    create-project
    added index.html and search-results.html

---
+++ source/js/script.js
@@ -0,0 +1,30 @@
+// Свайпер
+const swiperCourses = new Swiper('.swiper-courses', {
+    spaceBetween: 20,
+    slidesPerView: 4,
+  
+    navigation: {
+      nextEl: '.slider__btn-next',
+      prevEl: '.slider__btn-prev',
+    },
+});
+
+const swiperWebinar = new Swiper('.swiper-webinar', {
+    spaceBetween: 20,
+    slidesPerView: 4,
+  
+    navigation: {
+      nextEl: '.slider__btn-next',
+      prevEl: '.slider__btn-prev',
+    },
+});
+
+const swiperLecture = new Swiper('.swiper-lecture', {
+    spaceBetween: 20,
+    slidesPerView: 4,
+  
+    navigation: {
+      nextEl: '.slider__btn-next',
+      prevEl: '.slider__btn-prev',
+    },
+});
