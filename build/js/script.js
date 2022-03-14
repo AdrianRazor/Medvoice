e77bf01956b99255b5cd43852d38e3126f9148d5
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-03-14 10:43:29 +0200

    fixed-slider
    fixed sliders in index.html;
    fixed disabled links in navigation menu;
    changed buttons to links in guest mode in header;

--- build/js/script.js
+++ build/js/script.js
@@ -32,7 +32,7 @@ window.addEventListener('click', e => {
 // Свайпер
 const swiperCourses = new Swiper('.swiper-courses', {
     spaceBetween: 20,
-    slidesPerView: 4,
+    slidesPerView: 'auto',
   
     navigation: {
         nextEl: '.slider__btn-next',
@@ -42,7 +42,7 @@ const swiperCourses = new Swiper('.swiper-courses', {
 
 const swiperWebinar = new Swiper('.swiper-webinar', {
     spaceBetween: 20,
-    slidesPerView: 4,
+    slidesPerView: 'auto',
   
     navigation: {
         nextEl: '.slider__btn-next',
@@ -52,7 +52,7 @@ const swiperWebinar = new Swiper('.swiper-webinar', {
 
 const swiperLecture = new Swiper('.swiper-lecture', {
     spaceBetween: 20,
-    slidesPerView: 4,
+    slidesPerView: 'auto',
   
     navigation: {
         nextEl: '.slider__btn-next',
