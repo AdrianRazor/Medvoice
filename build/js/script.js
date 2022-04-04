6070c11e828a0939d8bde68c383db907e7eeb30e
Andy <53950662+AdrianRazor@users.noreply.github.com>
2022-04-04 12:10:43 +0300

    changed opening profile menu from click to hover

--- build/js/script.js
+++ build/js/script.js
@@ -16,23 +16,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
   // Выпадающее меню в шапке при клике на пользователя
-  let headerProfile = document.querySelector('.profile'); // находим кнопку для открытия/закрытия окна навигации
-  let headerProfileMenu = document.querySelector('.profile__menu'); // находим окно навигации
+  // let headerProfile = document.querySelector('.profile');
+  // let headerProfileMenu = document.querySelector('.profile__menu');
 
-  if (headerProfile) {
-    headerProfile.addEventListener('click', () => {
-      headerProfileMenu.classList.toggle('hidden');
-    })
-  }
+  // if (headerProfile) {
+  //   headerProfile.addEventListener('click', () => {
+  //     headerProfileMenu.classList.toggle('hidden');
+  //   })
+  // }
 
-  window.addEventListener('click', e => {
-    if (headerProfileMenu) {
-      const target = e.target;
-      if (!target.closest('.profile__menu') && !target.closest('.profile')) {
-        headerProfileMenu.classList.add('hidden');
-      }
-    }
-  })
+  // window.addEventListener('click', e => {
+  //   if (headerProfileMenu) {
+  //     const target = e.target;
+  //     if (!target.closest('.profile__menu') && !target.closest('.profile')) {
+  //       headerProfileMenu.classList.add('hidden');
+  //     }
+  //   }
+  // })
 
 
   // Свайпер
